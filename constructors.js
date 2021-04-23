adbe4d0b835741a6125992adae83ff1936dbf494
Noe Pinedo <noe@MacBook-Pro.local>
2021-04-22 19:23:30 -0600

    done

--- constructors.js
+++ constructors.js
@@ -15,87 +15,178 @@
 */
 
 // Code here
+function CarFactory(make, model) {
+  this.make = make;
+  this.model = model;
+}
+
 
 ////////// PROBLEM 2 //////////
 
 // Do not edit the code below.
 
 function Employee(name, email, hireDate) {
-    this.name = name;
-    this.email = email;
-    this.hireDate = hireDate;
+  this.name = name;
+  this.email = email;
+  this.hireDate = hireDate;
+}
+
+// Do not edit the code above.
+
+/*
+  Invoke the constructor function above and pass in 'Bob', 'bob@gmail.com', and '01-02-98' as arguments.
+  Assign the result of the invocation to a variable called bob.
+*/
+
+// Code here
+Employee('Bob', 'bob@gmail.com', '01-02-98')
+this.Bob = 'Bob';
+this.email = 'bob@gmail.com';
+this.hireDate = '01-02-98';
+
+
+
+let bob = new Employee('Bob', 'bob@gmail.com', '01-02-98')
+
+
+
+////////// PROBLEM 3 //////////
+
+// Do not edit the code below.
+
+var prius = new Car('Toyota', 'Prius', 2011);
+var mustang = new Car('Ford', 'Mustang', 2013);
+prius.moveCar(); // Increments prius' move property by 10. Returns the new move property.
+mustang.moveCar(); // Increments mustang' move property by 10. Returns the new move property.
+
+// Do not edit the code above.
+
+/*
+  Write a constructor function, including method definitions, which will make the above function invocations function properly.
+ 
+  Hint: you'll need to add a move property,
+    with a starting value of zero, and write a moveCar function which will increment the move property by 10.
+  The move property will be added to every object that is being returned from the Car function.
+  You'll also need to use the 'this' keyword properly in order to make sure you're
+    invoking moveCar on the right object (prius vs mustang).
+*/
+
+// Code here
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.move = 0;
+  this.moveCar = function () {
+    return this.move += 10
   }
-  
-  // Do not edit the code above.
-  
-  /*
-    Invoke the constructor function above and pass in 'Bob', 'bob@gmail.com', and '01-02-98' as arguments.
-    Assign the result of the invocation to a variable called bob.
-  */
-  
-  // Code here
-  
-  ////////// PROBLEM 3 //////////
-  
-  // Do not edit the code below.
-  
-  var prius = new Car('Toyota', 'Prius', 2011);
-  var mustang = new Car('Ford', 'Mustang', 2013);
-  prius.moveCar(); // Increments prius' move property by 10. Returns the new move property.
-  mustang.moveCar(); // Increments mustang' move property by 10. Returns the new move property.
-  
-  // Do not edit the code above.
-  
-  /*
-    Write a constructor function, including method definitions, which will make the above function invocations function properly.
-  
-    Hint: you'll need to add a move property,
-      with a starting value of zero, and write a moveCar function which will increment the move property by 10.
-    The move property will be added to every object that is being returned from the Car function.
-    You'll also need to use the 'this' keyword properly in order to make sure you're
-      invoking moveCar on the right object (prius vs mustang).
-  */
-  
-  // Code here
-  
-  ////////// PROBLEM 4 //////////
-  
-  /*
-    Here we have a constructor function named Movie that takes in 3 parameters:
-      name (a string), genre (a string), and rating (a number withing 1-100 that has been averaged out from previous given ratings).
-    Write a prototype method for the Movie constructor function called changeRating.
-    This method should take in a number as a parameter that will be a new rating.
-    Find the average between the old rating and the new rating.
-    Change the rating property to become this new number and return the updated rating.
-  */
-  
-  function Movie(name, genre, rating) {
-    this.name = name;
-    this.genre = genre;
-    this.rating = rating;
+}
+
+
+
+
+////////// PROBLEM 4 //////////
+
+/*
+  Here we have a constructor function named Movie that takes in 3 parameters:
+    name (a string), genre (a string), and rating (a number within 1-100 that has been averaged out from previous given ratings).
+  Write a prototype method for the Movie constructor function called changeRating.
+  This method should take in a number as a parameter that will be a new rating.
+  Find the average between the old rating and the new rating.
+  Change the rating property to become this new number and return the updated rating.
+*/
+
+
+// Code here
+function Movie(name, genre, rating) {
+  this.name = name;
+  this.genre = genre;
+  this.rating = rating;
+}
+Movie.prototype.changeRating = function (number) {
+  let rating = (this.rating + number) / 2
+  this.rating = rating
+  return rating
+}
+
+
+////////// PROBLEM 5 //////////
+
+/* Write a constructor function called User. This function should take in 4 parameters called name, age, email, and savedPosts in that order. Name and email will be strings, age will be a number and 
+savedPosts will be an array of objects. These objects will each have 3 properties: id (a number), title (a string), and rating (a number between 1 and 5). These objects are the posts that the user will have saved to their account.*/
+
+/* Once the User constructor function is created, write a prototype method for the User function. Name this method addSavedPost. 
+It should take in three parameters: id (a number), title (a string) and rating (a number). 
+Use these parameters to create a new object and add it to the savedPosts array.
+Make sure to name the properties the same as described previously (id, title, rating).*/
+
+// Code here
+function User(name, age, email, savedPosts) {
+  this.name = name;
+  this.email = email;
+  this.age = age;
+  this.savedPosts = savedPosts
+}
+// = [
+//   {
+//     id: number,
+//     title: 'string',
+//     rating: number2
+//   },
+
+//   {
+//     id: number,
+//     title: 'string',
+//     rating: number2
+//   },
+
+//   {
+//     id: number,
+//     title: 'string',
+//     rating: number2
+//   },
+
+// ]
+
+User.prototype.addSavedPost = function (id, title, rating) {
+  const newObj = {
+    id,
+    title,
+    rating
+
   }
-  
-  // Code here
-  
-  ////////// PROBLEM 5 //////////
-  
-  // Write a constructor function called User. This function should take in 4 parameters called name, age, email, and savedPosts in that order. Name and email will be strings, age will be a number and savedPosts will be an array of objects. These objects will each have 3 properties: id (a number), title (a string), and rating (a number between 1 and 5). These objects are the posts that the user will have saved to their account.
-  
-  // Once the User constructor function is created, write a prototype method for the User function. Name this method addSavedPost. It should take in three parameters: id (a number), title (a string) and rating (a number). Use these parameters to create a new object and add it to the savedPosts array. Make sure to name the properties the same as described previously (id, title, rating).
-  
-  // Code here
-  
-  ////////// PROBLEM 6 //////////
-  
-  // You will be using the constructor function you just created in problem 5.
-  // Write a prototype method for the User constructor function named removeSavedPost that will take in one number parameter representing the post id. Use this id to find and remove the matching object in the savedPosts array.
-  
-  // Code here
-  
-  ////////// PROBLEM 7 //////////
-  
-  // You will continue to use the constructor function you created in problem 5.
-  // Write a prototype method for the User constructor function named changePostRating that will take in two number parameters. The first will be an id (a number) and the second will be the new rating (a number). Use the id to find the matching object in the savedPosts array. Once you find the matching object, update it's rating score with the new rating parameter.
-  
-  // Code here
-  
+  this.savedPosts.push(newObj)
+}
+
+
+////////// PROBLEM 6 //////////
+
+/* You will be using the constructor function you just created in problem 5.
+ Write a prototype method for the User constructor function named removeSavedPost that will
+ take in one number parameter representing the post id. Use this id to find and remove the matching
+ object in the savedPosts array.*/
+
+// Code here
+User.prototype.removeSavedPost = function (id) {
+  const index = this.savedPosts.findIndex(function (post) {
+    return post.id === id
+  })
+  this.savedPosts.splice(index, 1)
+}
+
+
+
+
+
+// ////////// PROBLEM 7 //////////
+
+// // You will continue to use the constructor function you created in problem 5.
+// // Write a prototype method for the User constructor function named changePostRating that will take in two number parameters. 
+// //The first will be an id (a number) and the second will be the new rating (a number). Use the id to find the matching object 
+// //in the savedPosts array. Once you find the matching object, update it's rating score with the new rating parameter.
+
+// // Code here
+User.prototype.changePostRating = function (id, newRating) {
+  const index = this.savedPosts.findIndex((post) => post.id === id)
+  this.savedPosts[index].rating = newRating
+}
--- context.js
+++ context.js
@@ -18,11 +18,22 @@
 */
 
 //Code Here
+let user = {
+  username: 'string',
+  email: 'email',
+  getUsername: function () {
+    return this.username
+  }
+}
+
+
 
 ////////// PROBLEM 2 //////////
 
 /*
- Below we have the constructor Animal.  The eat method is using the "this" keyword.  Use the "new" keyword to assign context to "this", and save the instance to a variable named animal1.  You can pass anything you want in for name, species and food.
+ Below we have the constructor Animal.  The eat method is using the "this" keyword.  Use the "new" 
+ keyword to assign context to "this", and save the instance to a variable named animal1.  
+ You can pass anything you want in for name, species and food.
 */
 
 function Animal(name, species, food) {
@@ -30,7 +41,7 @@ function Animal(name, species, food) {
   this.species = species
   this.food = food
 
-  this.eat = function() {
+  this.eat = function () {
     return (
       this.name + ' is a ' + this.species + ' and likes to eat ' + this.food
     )
@@ -38,11 +49,14 @@ function Animal(name, species, food) {
 }
 
 //Code Here
+let animal1 = new Animal('joe', 'panda', 'banana')
+animal1.eat()
 
 ////////// PROBLEM 3 //////////
 
 /*
-  Use the bind method to assign context of the "this" keyword in the sayHi function to the user object; and save the bound function to a variable named whoSaysHi.  
+  Use the bind method to assign context of the "this" keyword in the sayHi function to the user object; and save 
+  the bound function to a variable named whoSaysHi.  
 */
 
 function sayHi(greeting) {
@@ -56,11 +70,12 @@ let who = {
 }
 
 //Code Here
-
+let whoSaysHi = sayHi.bind(who)
 ////////// PROBLEM 4 //////////
 
 /*
-  here we have a function that just returns the "this" keyword.  We will give context to "this", and your job is to tell us what the context is.
+  here we have a function that just returns the "this" keyword.  
+  We will give context to "this", and your job is to tell us what the context is.
 */
 
 function whatIsThis() {
@@ -68,7 +83,7 @@ function whatIsThis() {
 }
 
 // uncomment the line below and tell us what the context of "this" is for whatIsThis()
-//let context1 = ???
+let context1 = window
 
 let product = {
   name: 'snake plant',
@@ -80,7 +95,7 @@ let product = {
 let func = whatIsThis.bind(product)
 
 // uncomment the line below and tell us what the context of "this" is when we invoke func
-//let context2 = ???
+let context2 = product
 
 let vacation = {
   location: 'Hawaii',
@@ -91,14 +106,14 @@ let vacation = {
 }
 
 // uncomment the line below and tell us what the context of "this" is when we invoke vacation.whatIsThis
-//let context3 = ???
+let context3 = vacation
 
 function Family(numParents, numKids, numPets) {
   this.numParents = numParents
   this.numKids = numKids
   this.numPets = numPets
 
-  this.whatIsThis = function() {
+  this.whatIsThis = function () {
     return this
   }
 }
@@ -106,4 +121,4 @@ function Family(numParents, numKids, numPets) {
 let family1 = new Family(2, 4, 1)
 
 // uncomment the line below and tell us what the context of "this" is for the instance of Family created above.
-// let context4 = ???
+let context4 = family1
